861a57fffa0aa8b1ddd09e517bb492b8582db239
Logan Power <lmpynix@gmail.com>
2022-04-04 13:35:59 -0500

    Update command.c

--- src/command.c
+++ src/command.c
@@ -151,7 +151,6 @@ bool cmd_version(target *t, int argc, char **argv)
 	gdb_out("Copyright (C) 2015  Black Sphere Technologies Ltd.\n");
 	gdb_out("License GPLv3+: GNU GPL version 3 or later "
 		"<http://gnu.org/licenses/gpl.html>\n\n");
-#endif
 
 	return true;
 }
